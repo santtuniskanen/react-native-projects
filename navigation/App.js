f4bcbc11cc40f58011ff34c46e7efade0cd2065e
santtuniskanen <santtuniska02@gmail.com>
2024-01-23 12:33:47 +0200

    navigation exercise

---
+++ navigation/App.js
@@ -0,0 +1,33 @@
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from './HomeScreen'
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Home",
+            headerTitle: "Home",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
